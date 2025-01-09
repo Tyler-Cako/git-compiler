70ecc29b1a47236735c154304cd033690c1bbe1d
Tyler <tylercakotc@gmail.com>
2025-01-09 08:01:05 -0700

    .

--- repl/repl.go
+++ repl/repl.go
@@ -3,6 +3,7 @@ package repl
 import (
 	"bufio"
 	"fmt"
+	"go-compiler/evaluator"
 	"go-compiler/lexer"
 	"go-compiler/parser"
 	"io"
@@ -43,8 +44,11 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 
